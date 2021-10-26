d792580f046943718926a6900fe6cce3fd8fa378
ebad-k <92480224+ebad-k@users.noreply.github.com>
2021-10-27 02:45:45 +0500

    Update Login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,41 +4,12 @@ import Button from "@ant-design/react-native/lib/button";
 import Card from "@ant-design/react-native/lib/card";
 export const Login = ({ history }) => {
   return (
-    <View
-      style={{
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Card
-        style={{
-          backgroundColor: "#d9d9d9",
-          border: "1px solid black",
-          width: "400px",
-          height: "500px",
-          marginTop: "70px",
-        }}
-      >
-        <View style={styles.container}>
-          <Text
-            style={{
-              textAlign: "center",
-              fontSize: "45px",
-              marginTop: 100,
-              fontWeight: "bold",
-            }}
-          >
-            Instagram
-          </Text>
+    <View style={styles.container}>
+      <Card style={styles.cardStyle}>
+        <View>
+          <Text style={styles.textStyle}>Instagram</Text>
           <Image
-            style={{
-              width: 100,
-              height: 100,
-              justifyContent: "center",
-              alignContent: "center",
-              marginLeft: "37%",
-              marginTop: "20px",
-            }}
+            style={styles.logo}
             source={{
               uri: "https://www.freepnglogos.com/uploads/logo-ig-png/logo-ig-stunning-instagram-logo-vector-download-for-new-7.png",
             }}
@@ -46,12 +17,7 @@ export const Login = ({ history }) => {
         </View>
         <Button
           title="Login With Instagram"
-          style={{
-            color: "blue",
-            width: "300px",
-            marginLeft: "13%",
-            marginTop: "30%",
-          }}
+          style={styles.buttonStyle}
           type="primary"
           onPress={() =>
             Linking.openURL(
@@ -68,9 +34,36 @@ export const Login = ({ history }) => {
 
 const styles = StyleSheet.create({
   container: {
-    // paddingLeft: "10rem",
-    // paddingTop: " 20%",
-    // fontSize: "30px",
-    // borderBottomColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cardStyle: {
+    backgroundColor: "#d9d9d9",
+    border: "1px solid black",
+    width: "400px",
+    height: "500px",
+    marginTop: "78px",
+    marginBottom: "79px",
+  },
+  textStyle: {
+    textAlign: "center",
+    fontSize: "45px",
+    marginTop: 100,
+    fontWeight: "bold",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    justifyContent: "center",
+    alignContent: "center",
+    marginLeft: "37%",
+    marginTop: "20px",
+  },
+  buttonStyle: {
+    color: "blue",
+    width: "300px",
+    marginLeft: "13%",
+    marginTop: "30%",
   },
 });
+
